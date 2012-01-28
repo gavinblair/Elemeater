a2b2bd91bfb54b60e1defa8604ac13fc7b86372b
Andy Berdan <andy@berdan.ca>
2012-01-28 11:50:45 -0500

    Added: bare bones ring.js

---
+++ js/ring.js
@@ -0,0 +1,14 @@
+class Ring
+{
+	public function getPosOfSnake(var index:int):Object
+	{
+		/*
+			Snakes positioned on a circle. 0-index at 12 o'clock
+		*/
+
+		var radius:int = 100;
+		var numElements:int = 24;
+
+		return { x:0, y:0 };
+	}
+};
