b0cb5f8ba1566c374ea8e35cb2803848ca227b2c
gavinblair <gavin.blair@rtraction.com>
2012-01-28 13:53:33 -0500

    sprites! defining animations

--- js/script.js
+++ js/script.js
@@ -6,6 +6,64 @@ $(document).ready(function(){
 
 	$("#playground").playground({height: PLAYGROUND_HEIGHT, width: PLAYGROUND_WIDTH});
 	
+	//define animations
+	
+	//big tail
+	var bigtail = new $.gameQuery.Animation({ 
+		imageURL: "img/bigtail.png",
+		numberOfFrame: 1,
+		delta: 100,
+		rate: 1,
+		type: $.gameQuery.ANIMATION_VERTICAL
+	});
+	
+	//big head
+	var bighead = new $.gameQuery.Animation({ 
+		imageURL: "img/bighead.png",
+		numberOfFrame: 1,
+		delta: 100,
+		rate: 1,
+		type: $.gameQuery.ANIMATION_VERTICAL
+	});
+	
+	//big body
+	var bigbody = new $.gameQuery.Animation({ 
+		imageURL: "img/bigbody.png",
+		numberOfFrame: 1,
+		delta: 100,
+		rate: 1,
+		type: $.gameQuery.ANIMATION_VERTICAL
+	});
+	
+	//double snake
+	var doublesnake = new $.gameQuery.Animation({ 
+		imageURL: "img/doubles.png",
+		numberOfFrame: 1,
+		delta: 100,
+		rate: 1,
+		type: $.gameQuery.ANIMATION_HORIZONTAL
+	});
+	//offsety*100 will be a different snake
+	
+	//single snake
+	var doublesnake = new $.gameQuery.Animation({ 
+		imageURL: "img/doubles.png",
+		numberOfFrame: 1,
+		delta: 70,
+		rate: 1,
+		type: $.gameQuery.ANIMATION_HORIZONTAL
+	});
+	//offsety*70 will be a different snake //do we need to multiply this?
+	
+	//exploding snake
+	
+	//add the ring group
+	$.playground().addGroup("ring", {
+		width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT
+	});
+	$.playground().addSprite("doublesnake", {animation: doublesnake, width: 100, height: 100});
+	$.playground().startGame();
+	
 	var tiles = new Array();
 	
 	for(var i  = 0; i <= SNAKES_IN_RING; i++) {
@@ -30,7 +88,24 @@ $(document).ready(function(){
 		$("#playground").addSprite("tile"+i,{animation: tiles[i]})
 		
 	}
+	
+	// this sets the id of the loading bar:
+	$().setLoadBar("loadingBar", 400);
+	
+	//initialize the start button
+	$("#startbutton").click(function(){
+		$.playground().startGame(function(){
+			$("#welcomeScreen").fadeTo(1000,0,function(){$(this).remove();});
+		});
+	});
 });
+
+// Function to restart the game:
+function restartgame(){
+	window.location.reload();
+};
+
+
 
 
 
